8c4f5d633b354172525e5539fb251cd11df1ee38
Chahine-Chahine <chahinesquare@hotmail.com>
2024-07-27 20:26:30 +0300

    implementing hero background with text centered on grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import { Spotlight } from './ui/Spotlight'
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div className="pb-20 pt-36">
       <div>
-        <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
-        <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
-        <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8"/>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute left-0 top-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8" />
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dinamic web magic with nextjs
+          </h2>
+        </div>
+      </div>
     </div>
-    <div className='flex justify-center'>
-<div className='max-w-[80vw]'>
-  <h1>Dinamic web magic with nextjs</h1>
-</div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
