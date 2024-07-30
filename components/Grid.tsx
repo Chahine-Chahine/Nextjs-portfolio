23e85f531b89c9e53f7a172f98f40309bd585ed5
Chahine-Chahine <chahinesquare@hotmail.com>
2024-07-30 15:48:09 +0300

    implementing animated cards with data file to fill

---
+++ components/Grid.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/bento-grid'
+import { gridItems } from '@/Data'
+
+const Grid = () => {
+  return (
+    <section id='about'>
+    <BentoGrid>
+        {gridItems.map((item , i)=>(<BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description}/>))}
+    </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
