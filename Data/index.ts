1165e2fde8cdc88f64484552c82ca12cbd161598
Chahine-Chahine <chahinesquare@hotmail.com>
2024-08-01 23:07:46 +0300

    implementing testemonials section with infinite cards animation

--- Data/index.ts
+++ Data/index.ts
@@ -107,33 +107,38 @@ export const navItems = [
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Collaborating with Chahine was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Chahine's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Anfs Designs",
+      title: "Degital Marketing Agency",
+      image: "/profile.svg"
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
+      "Collaborating with Chahine was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Chahine's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Anfs Designs",
       title: "Director of AlphaStream Technologies",
+      image: "/stream.svg"
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
+      "Collaborating with Chahine was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Chahine's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Anfs Designs",
       title: "Director of AlphaStream Technologies",
+      image: "/stream.svg"
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
+      "Collaborating with Chahine was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Chahine's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Anfs Designs",
       title: "Director of AlphaStream Technologies",
+      image: "/stream.svg"
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
+      "Collaborating with Chahine was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Chahine's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Anfs Designs",
       title: "Director of AlphaStream Technologies",
+      image: "/stream.svg"
     },
   ];
   
