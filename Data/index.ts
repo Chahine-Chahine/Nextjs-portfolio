156117ed3ff840c6f2d434b7ed284ea745472795
Chahine-Chahine <chahinesquare@hotmail.com>
2024-08-02 02:50:44 +0300

    implementing experience section and moving borders component

--- Data/index.ts
+++ Data/index.ts
@@ -176,34 +176,34 @@ export const navItems = [
   ];
   
   export const workExperience = [
-    {
-      id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-      className: "md:col-span-2",
-      thumbnail: "/exp1.svg",
-    },
-    {
-      id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", 
-      thumbnail: "/exp2.svg",
-    },
-    {
-      id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", 
-      thumbnail: "/exp3.svg",
-    },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
+      {
+        id: 1,
+        title: "Frontend Developer",
+        desc: "Built responsive web applications using React.js for various clients, enhancing user interface design and improving user experience.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+      },
+      {
+        id: 2,
+        title: "Backend Developer",
+        desc: "Designed and implemented server-side logic and APIs using Node.js or laravel for diverse projects, ensuring seamless data integration and app performance.",
+        className: "md:col-span-2",
+        thumbnail: "/exp2.svg",
+      },
+      {
+        id: 3,
+        title: "Database Specialist",
+        desc: "Managed and optimized databases using SQL and NoSQL technologies for multiple clients, ensuring data security and high availability.",
+        className: "md:col-span-2",
+        thumbnail: "/exp3.svg",
+      },
+      {
+        id: 4,
+        title: "AI Developer",
+        desc: "Developed mobile application with OpenAI integration for artificial assistance in decision making.",
+        className: "md:col-span-2",
+        thumbnail: "/exp4.svg",
+      },
   ];
   
   export const socialMedia = [
