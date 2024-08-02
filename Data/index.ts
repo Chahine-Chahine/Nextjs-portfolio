c0f264f42d2abfb17eaf3c58502357b614376d39
Chahine-Chahine <chahinesquare@hotmail.com>
2024-08-02 04:18:21 +0300

    implementing approach section and Canvas reveal effect

--- Data/index.ts
+++ Data/index.ts
@@ -72,9 +72,9 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
+      title: "Roam Rover Trip Planer",
+      des: "It simplifies the decision-making process by leveraging AI for top recommendations.",
+      img: "/RoamRover.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
       link: "https://github.com/adrianhajdin?tab=repositories",
     },
