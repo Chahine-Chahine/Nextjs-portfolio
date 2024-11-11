f2c4fdf2a45e1b68ad7cff79c4b6856b46279d7a
Chahine Chahine <138026169+Chahine-Chahine@users.noreply.github.com>
2024-11-11 04:19:56 +0200

    implementing fixes to the layout

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+   <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto w-full sm:px-10 px-5 overflow-x-hidden">
     <div className="max-w-7xl w-full">
       <FloatingNav navItems={navItems}/>
       <Hero/>
