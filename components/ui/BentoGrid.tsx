cdfbe8888ba996e47a9bacecfeba5aca5a5bc977
Chahine Chahine <138026169+Chahine-Chahine@users.noreply.github.com>
2025-01-04 10:57:15 +0200

    modified bentogrid for test improvement

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
 
 import Lottie from "react-lottie";
 
@@ -93,10 +94,11 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
-              className={cn(imgClassName, "object-cover object-center ")}
+              fill
+              className={cn(imgClassName, "object-cover object-center")}
             />
           )}
         </div>
@@ -105,10 +107,11 @@ export const BentoGridItem = ({
             } `}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
-              className="object-cover object-center w-full h-full"
+              fill
+              className="object-cover object-center"
             />
           )}
         </div>
