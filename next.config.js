60880fe134150ce3a37cb324e4cd7795e4e4a5b6
Chahine Chahine <138026169+Chahine-Chahine@users.noreply.github.com>
2025-01-04 11:04:28 +0200

    deleted nextconfig.js

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  images: {
-    domains: ['your-image-domain.com'], // Add your image domains here
-  },
-} 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['kreativebrains.com'], 
+  },
+};
 
 export default nextConfig;
